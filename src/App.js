3067be21ab1820526c96b46c692a70a06b92082d
Max <TheZnat7@yandex.ru>
2023-10-06 00:59:32 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import './App.css';
+import React from 'react';
+import './scss/app.scss';
+
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Тест</h1>
+   return(
+       <div className="wrapper">
+           <Header />
+           <div className="content">
+               <div className="container">
+                   <div className="content__top">
+                       <Categories/>
+                       <Sort/>
+                   </div>
+                   <h2 className="content__title">Все пиццы</h2>
+                   <div className="content__items">
+                      <PizzaBlock
+                        title="Чизбургер-пицца"
+                        price={395}
+                      />
+                       <PizzaBlock
+                           title="Чизбургер-пицца"
+                           price={395}
+                       />
+                       <PizzaBlock
+                           title="Чизбургер-пицца"
+                           price={395}
+                       />
+                       <PizzaBlock
+                           title="Чизбургер-пицца"
+                           price={395}
+                       />
+                       <PizzaBlock
+                           title="Чизбургер-пицца"
+                           price={395}
+                       />
+                   </div>
+               </div>
+           </div>
+       </div>
+   );
 
-    </div>
-  );
+
 }
 
 export default App;
